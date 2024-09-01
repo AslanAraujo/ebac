898a78ec2216f01e3238aec6b20d6114d7bc3f59
AslanAraujo <aslan.andrade@hotmail.com>
2024-09-01 14:51:46 +0000

    Arquivos de preço da gasolina, script Python e gráfico criados na branch DEVEL

--- gasolina.py
+++ gasolina.py
@@ -1,18 +1,20 @@
 
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
-#Carregando os dados
+# Carregando os dados
 df = pd.read_csv('gasolina.csv')
 
 # Plotando dados
 plt.figure(figsize=(8, 6))
-plt.plot(df['dia'], df['venda'], marker='o', linestyle='-')
-plt.title('Variação do Preço da Gasolina ao Longo dos Dias')
+plt.plot(df['dia'], df['venda'], marker='o', linestyle='-', color='darkgreen', label='Preço da Gasolina') # alterrei cor verde na linha
+
+# Ajustando o título para negrito e adicionando a legenda
+plt.title('Variação do Preço da Gasolina ao Longo dos Dias', fontweight='bold') #adicionando negrito no titulo
 plt.xlabel('Dia')
 plt.ylabel('Preço (R$)')
 plt.grid(True)
 
-#salvando grafico
+# Salvando o gráfico
 plt.savefig('gasolina.png')
 plt.show()
